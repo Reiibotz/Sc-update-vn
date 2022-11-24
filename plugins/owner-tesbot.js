5cd8097d1df668d572231192bd93fe66af48e3e2
Reiibotz <topuprainalz@gmail.com>
2022-11-24 21:37:38 +0700

    first commit

---
+++ plugins/owner-tesbot.js
@@ -0,0 +1,37 @@
+import fetch from 'node-fetch'
+import fs from 'fs';
+import crypto from 'crypto'
+let handler = async (m, { conn, text, usedPrefix }) => {
+    const ultah = new Date('November 4 2022 23:59:59')
+    const sekarat = new Date().getTime() 
+    const Kurang = ultah - sekarat
+    const ohari = Math.floor( Kurang / (1000 * 60 * 60 * 24));
+    const ojam = Math.floor( Kurang % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+    const onet = Math.floor( Kurang % (1000 * 60 * 60) / (1000 * 60))
+    const detek = Math.floor( Kurang % (1000 * 60) / 1000)
+    let sewa = `*🎏 Bot Aktif Kak*
+
+${wm}
+${botdate}
+`
+        let img1 = fs.readFileSync('./thumbnail.jpg');
+        let img2 = fs.readFileSync('./media/bawah.png');
+    return conn.sendButton(m.chat, hiasan, sewa, await (await fetch(`https://telegra.ph/file/6ab4daac226292a112540.jpg`)).buffer(), [["🗳️MENU", usedPrefix + "menu"], ["🔭DONASI", usedPrefix + "donasi"]], m, {
+        contextInfo: {
+            externalAdReply: {
+                mediaUrl: '',
+                        mediaType: 2,
+                        description: 'anu',
+                        title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
+                        body: `Follow TT My Bestie`,                                       previewType: 0,
+                        thumbnail: await (await fetch(`https://i.ibb.co/jfZVKmC/babi2.jpg`)).buffer(),
+                        sourceUrl: 'https://www.tiktok.com/@always_ntreii?_t=8VvI63a8xaz&_r=1'
+            }
+        }
+    })
+}
+handler.help = ['tesbot']
+handler.tags = ['owner']
+handler.command = ['tesbot']
+handler.owner = true
+export default handler
---
+++ plugins/payment.js
@@ -0,0 +1,37 @@
+import fetch from 'node-fetch'
+
+//Plugin By Xynoz!!
+let handler = async (m, { conn, usedPrefix }) => {
+  const ultah = new Date('November 4 2022 00:00:01')
+    const sekarat = new Date().getTime() 
+    const Kurang = ultah - sekarat
+    const ohari = Math.floor( Kurang / (1000 * 60 * 60 * 24));
+    const ojam = Math.floor( Kurang % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+    const onet = Math.floor( Kurang % (1000 * 60 * 60) / (1000 * 60))
+    const detek = Math.floor( Kurang % (1000 * 60) / 1000)
+  let vn = `https://github.com/saipulanuar/Api-Github/raw/main/audio/Donasiku.mp3`
+  conn.sendButton(m.chat, `
+┏━ꕥ〔 *Donasi • Dana* 〕ꕥ━⬣
+┃✾ Gopay  [087778405247]
+┃✾ Dana  [0887778405247]
+┃✾ Saweria  [-]
+┗━━━━ꕥ
+┏━━ꕥ〔 *NOTE* 〕ꕥ━⬣
+┃ Ingin donasi? Wa.me/6287778405247
+┃ _Hasil donasi akan digunakan buat sewa_
+┃ _atau beli *Ranel* agar bot bisa jalan_
+┃ _24jam tanpa kendala_
+┗━━━━ꕥ
+`.trim(), wm, [['Owner',`/owner`]],m) // Tambah sendiri kalo mau
+conn.sendFile(m.chat, vn, 'haori.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true, contextInfo:{ externalAdReply: { title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`, body: `Follow Tiktok My Bestie`, sourceUrl: 'https://www.tiktok.com/@always_ntreii?_t=8VvI63a8xaz&_r=1', thumbnail: await (await fetch('https://i.ibb.co/xmZJY17/20221123-104432.png')).buffer(),}} 
+     }) 
+}
+handler.command = /^(pay|payment|bayar)$/i
+handler.tags = ['info']
+handler.help = ['payment']
+handler.premium = false
+handler.limit = true
+
+export default handler
